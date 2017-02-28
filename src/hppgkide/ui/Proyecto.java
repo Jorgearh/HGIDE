e5c8bfdfee43543b1761ba2c76f13dba45064f54
JorgeARH <jorge.arh88@gmail.com>
2017-02-27 20:14:10 -0600

    Funciones del usuario del lado del cliente parcialmente terminadas

---
+++ src/hppgkide/ui/Proyecto.java
@@ -0,0 +1,20 @@
+package hppgkide.ui;
+
+/**
+ *
+ * @author JorgeAntonio
+ */
+public class Proyecto {
+    
+    public static void nuevo(){
+          
+    }
+    
+    public static void importar(){
+        
+    }
+    
+    public static void publicar(){
+        
+    }
+}
